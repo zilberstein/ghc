3c660341647cb78db461cf80a34880d84b00092f
Simon Peyton Jones <simonpj@microsoft.com>
2018-03-22 14:37:35 +0000

    Add regression test for Trac #14961
    More precisely, for the code in comment:3

--- testsuite/tests/quantified-constraints/T9123a.hs
+++ testsuite/tests/quantified-constraints/T9123a.hs
@@ -3,14 +3,14 @@
            , UndecidableInstances
            , GeneralizedNewtypeDeriving #-}
 
-module T9123 where
+module T9123a where
 
 import Data.Coerce
 
 class MyMonad m where
   join :: m (m a) -> m a
 
-data StateT s m a = StateT (s -> m (a, s))   -- could be a newtype, but that 
+data StateT s m a = StateT (s -> m (a, s))   -- could be a newtype, but that
                                              -- doesn't change my argument
 
 type role StateT nominal representational nominal   -- as inferred
@@ -19,8 +19,8 @@ instance Monad m => Monad (StateT s m) where
 newtype IntStateT m a = IntStateT (StateT Int m a)
   deriving Monad
 
-deriving instance (Applicative m, forall a. Coercible a b => Coercible (m a) (m b)) 
+deriving instance (Applicative m, forall a. Coercible a b => Coercible (m a) (m b))
                => Applicative (IntStateT m)
 
-deriving instance (Monad m, forall a. Coercible a b => Coercible (m a) (m b)) 
+deriving instance (Monad m, forall a. Coercible a b => Coercible (m a) (m b))
                => Monad (IntStateT m)
